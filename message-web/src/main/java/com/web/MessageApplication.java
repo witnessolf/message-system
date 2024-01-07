91ac4c895577db5359820f8d10ea448f27304472
witnessolf <1713157566@qq.com>
2024-01-07 22:19:01 +0800

    日志、pom文件初始化

---
+++ message-web/src/main/java/com/web/MessageApplication.java
@@ -0,0 +1,16 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 17131
+ * @Date 2024/1/7
+ * @Description:
+ */
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+}
